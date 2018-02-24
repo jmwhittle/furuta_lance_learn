362b47386e4094d67aa8bc0e28f2d4c2a58e9d47
jasonwhittle <jasonwhittle@lavabit.com>
2018-02-23 21:19:51 -0700

    added some values from the matlab print out

--- runga_kutta.py
+++ runga_kutta.py
@@ -24,3 +24,22 @@ while t <= 10:
     if abs(round(t) - t) < 1e-5:
         print("y(%2.1f)\t= %4.6f \t error: %4.6g" % (t, y, abs(y - theory(t))))
     t, y = t + dt, y + dy(t, y, dt)
+
+# possible alternative solution https://github.com/MechCoder/Rk4-Polynomials/blob/master/rk4.py
+
+m1 = 0.2
+m1 = m2
+L1 = 6*0.0254
+L2 = L1
+l1 = L1/2
+l2 = L2/2
+g = 9.81
+
+b1 = 0.001
+b2 = 0.001
+J1 = m1*L1^(2/3)
+J2 = m2*L2^(3/3)
+
+J0_h = J1 + m1*l1^2 + m2*L1^2
+J1_h = J1 + m1*l1^2
+J2_h = J2 + m2*l2^2
