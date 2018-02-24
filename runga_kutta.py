2e6e24c63b367b0fdd70565b3ff2ec79cbb3ea4e
jasonwhittle <jasonwhittle@lavabit.com>
2018-02-23 21:47:49 -0700

    added some of the differential eqs

--- runga_kutta.py
+++ runga_kutta.py
@@ -27,6 +27,8 @@ while t <= 10:
 
 # possible alternative solution https://github.com/MechCoder/Rk4-Polynomials/blob/master/rk4.py
 
+import math
+
 m1 = 0.2
 m1 = m2
 L1 = 6*0.0254
@@ -35,6 +37,7 @@ l1 = L1/2
 l2 = L2/2
 g = 9.81
 
+Tao = 0.1*sin(5*t)
 b1 = 0.001
 b2 = 0.001
 J1 = m1*L1^(2/3)
@@ -43,3 +46,19 @@ J2 = m2*L2^(3/3)
 J0_h = J1 + m1*l1^2 + m2*L1^2
 J1_h = J1 + m1*l1^2
 J2_h = J2 + m2*l2^2
+
+t1 =
+t2 =
+t1_d =
+t2_d =
+
+# B_dd EQNS
+F = (-sin(2*t2)*J2_h^(2*t1_d*t2_d)) - (
+    L1*(sin(t2) - sin(t2)^3)*J2_h*l2*m2*t1_d^2) + (
+    L1*sin(t2)*J2_h*12*m2*t2_d^2) - (
+    b1-J2_h*t1_d) + (
+    Tao*J2_h) + (
+    L1*g*sin(2*t2)*l2^2*m2^(2/2)) + (
+    L1*b2*cos(t2)*l2*m2*t2_d)
+
+G = J2_h^2*sin(t2)^2 + J0_h
